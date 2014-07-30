bff52a4de495d30a0e89523de8413ffcf729f75b
Dean Kerr <dean_k@live.co.uk>
2014-07-30 22:43:19 +0100

    Fixed issue with jasmine tests

--- test/spec/controllers/expenseList.js
+++ test/spec/controllers/expenseList.js
@@ -1,22 +1,22 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: ExpenseListCtrl', function () {
 
   // load the controller's module
   beforeEach(module('cashflowTracker'));
 
-  var MainCtrl,
+  var ExpenseListCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    ExpenseListCtrl = $controller('ExpenseListCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have a default set of day expenses', function () {
+    expect(scope.dayExpenses.length).toBe(2);
   });
 });
